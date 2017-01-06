832cba872c5be920e8f93ece75d6d3a0f9a14772
doronrot <doronrot17@gmail.com>
2017-01-06 15:58:48 +0200

    324/324 tests passed !!!

--- compiler.scm
+++ compiler.scm
@@ -100,8 +100,12 @@
 		(if (or (null? parsed_exp)(atom? parsed_exp))
 			parsed_exp
 			(if (is_redundant? parsed_exp)	;are you applic lambda-nil expression?
-				(let ((lambda_nil_body (remove-applic-lambda-nil-helper parsed_exp)))
-				   (remove-applic-lambda-nil  lambda_nil_body))
+				(let ((lambda_nil_body (remove-applic-lambda-nil-helper parsed_exp))
+					  (applic_args (find_applic_args parsed_exp)))
+					(if(null? applic_args)
+					   (remove-applic-lambda-nil  lambda_nil_body)
+				   	   ;`(applic ,(remove-applic-lambda-nil  lambda_nil_body) ,applic_args)))
+					   parsed_exp))
 				(cons (remove-applic-lambda-nil  (car parsed_exp))
 					  (remove-applic-lambda-nil  (cdr parsed_exp)))))))
 
@@ -118,6 +122,10 @@
 				 (equal? (cadadr parsed_exp) (list)))
 			#t
 			#f)))
+
+(define find_applic_args
+	(lambda (applic_parsed_exp)
+		(caddr applic_parsed_exp)))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Box-Set ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -153,21 +161,56 @@
                      (is_exist_bound? lambda_body lambda_var))
                 #t
                 #f))))
-                
+ 
+
 (define is_exist_get?
     (lambda (exp_part suspected_var)
          (cond ((null? exp_part) #f)
+         	   ;is var
                ((equal? exp_part `(var ,suspected_var)) #t)
                ((atom? exp_part) #f)
+               ;set exp
                ((equal? (car exp_part) 'set) 
                      ;(equal? (cadadr exp_part) suspected_var)
-                  (is_exist_get? (caddr exp_part) suspected_var))
+                  (is_exist_get? (caddr exp_part) suspected_var)) ;caddr= set exp
+               ;lambda parameter
                ((and (is_lambda_exp? exp_part)
                      (member suspected_var (find_lambda_vars_op exp_part)))
                   #f)
-               (else (or (is_exist_get? (cdr exp_part) suspected_var)
-                         (is_exist_get? (car exp_part) suspected_var))))))
+               (else (or (is_exist_get? (car exp_part) suspected_var)
+                         (is_exist_get? (cdr exp_part) suspected_var))))))
 
+; (define is_exist_get?
+;     (lambda (exp_part suspected_var)
+;          (cond ((null? exp_part) 
+;          		   (begin (display 'null) (newline) #f))
+;          	   ;is var
+;                ((equal? exp_part `(var ,suspected_var)) 
+;                	   (begin (display 'is_var)(newline) 
+;                	   		  (display suspected_var) (newline) 
+;                	   		  #t))
+;                ((atom? exp_part) 
+;                	   (begin (display 'atom)(newline) 
+;                	   		  (display exp_part) (newline)
+;                	   		  #f))
+;                ;set exp
+;                ((equal? (car exp_part) 'set) 
+;                      ;(equal? (cadadr exp_part) suspected_var)
+;               	   (begin (display 'set)(newline)
+;               	   		  (display exp_part)(newline)
+;    	   	                  (is_exist_get? (caddr exp_part) suspected_var)) ;caddr= set exp
+; 				)
+;                ;lambda parameter
+;                ((and (is_lambda_exp? exp_part)
+;                      (member suspected_var (find_lambda_vars_op exp_part)))
+;                	   (begin (display 'lambda)(newline)
+;                	   		  (display exp_part)(newline)
+;                	   		  #f))
+;                (else (begin (display 'else) (newline)
+;                				(display exp_part) (newline)
+;                				(or (is_exist_get? (car exp_part) suspected_var)
+;                          	    (is_exist_get? (cdr exp_part) suspected_var)))))))
+; (put_set_boxes (cdr exp_part))))
                     
 (define is_exist_set?
      (lambda (exp_part suspected_var)
@@ -228,7 +271,8 @@
               ((and (equal? (car exp_part) 'set) 
                     (equal? (cadadr exp_part) suspected_var))
               	 (cons 'box-set
-              		   (cdr exp_part))) 
+              		   ;(cdr exp_part)))
+              		   (put_set_boxes (cdr exp_part) suspected_var)))
 			  ((and (is_lambda_exp? exp_part)
                     (member suspected_var (find_lambda_vars_op exp_part)))
 			     exp_part)
@@ -371,7 +415,8 @@
 (define find_lambda_vars_op
 	(lambda (lambda_expr)
 		 (cond ((equal? (car lambda_expr) 'lambda-opt)
-				  (list (caadr lambda_expr) (caddr lambda_expr)))  
+				  `(,@(cadr lambda_expr) ,(caddr lambda_expr)))  
+		 			; (append (cadr lambda_expr) (list (caddr lambda_expr))))
 			   ((equal? (car lambda_expr) 'lambda-simple)
 					   (cadr lambda_expr))
 			   (else (list (cadr lambda_expr))))))
@@ -422,17 +467,23 @@
 			  	 	`(if3 ,(annotate-tc-helper test #f) 
 			  	 		  ,(annotate-tc-helper dit tp?)
 			  	 		  ,(annotate-tc-helper dif tp?))))
-			  ;def
-			  ((equal? (car parsed_exp) 'def)
-			  	 (let ((def_var (cadr parsed_exp))
-			  	 	   (def_exp (caddr parsed_exp)))
-			  	 	`(def ,def_var ,(annotate-tc-helper def_exp #f))))
 			  ;lambda
 			  ((is_lambda_exp? parsed_exp)
 			  	 (let ((lambda_type (car parsed_exp))
 			  	 	   (lambda_vars (find_lambda_vars_show  parsed_exp))
 			  	 	   (lambda_body (car (find_lambda_body  parsed_exp))))
 			  	 	`(,lambda_type ,@lambda_vars ,(annotate-tc-helper lambda_body #t))))
+			  ;def
+			  ((equal? (car parsed_exp) 'def)
+			  	 (let ((def_var (cadr parsed_exp))
+			  	 	   (def_exp (caddr parsed_exp)))
+			  	 	`(def ,def_var ,(annotate-tc-helper def_exp #f))))
+			  ;set
+			  ((or (equal? (car parsed_exp) 'set)
+			  	   (equal? (car parsed_exp) 'box-set))
+			  	 (let ((set_var (cadr parsed_exp))
+			  	 	   (set_exp (caddr parsed_exp)))
+			  	 	`(,(car parsed_exp) ,set_var ,(annotate-tc-helper set_exp #f))))
 			  (else 
 			  	 (cons (annotate-tc-helper (car parsed_exp) tp?)
 			  	 	   (annotate-tc-helper (cdr parsed_exp) tp?)))
